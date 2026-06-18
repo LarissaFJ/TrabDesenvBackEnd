6b0b75e437e6676f9c0154540ff5bb19dde5b202
Larissa Ferreira <larissafj@gmail.com>
2026-06-18 17:01:35 -0300

    Primeiro commit

---
+++ src/main/java/com/example/trabdesenvbackend/TrabDesenvBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabdesenvbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabDesenvBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabDesenvBackEndApplication.class, args);
+    }
+
+}
